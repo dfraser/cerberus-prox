aee1a0b5f52a9f1b8bfc87ebf9326d991b9f3c95
dfraser <dfraser@b397fa25-8354-0410-bb84-f944691230c9>
2010-03-25 00:55:35 +0000

    Hardware Revision A (original)

--- src/com/onestopmediagroup/doorsecurity/DoorAccessListener.java
+++
@@ -1,7 +0,0 @@
-package com.onestopmediagroup.doorsecurity;
-
-import java.util.EventListener;
-
-public interface DoorAccessListener extends EventListener {
-	void doorActionEvent(DoorAccessEvent event);
-}
